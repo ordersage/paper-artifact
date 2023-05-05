6ee81bf4ca1eeb2354aee83f2b22f732046474cc
Robert Ricci <ricci@cs.utah.edu>
2023-05-05 16:08:04 -0600

    Initial version of Ordersage profile

--- profile.py
+++ profile.py
@@ -1,27 +1,45 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
+"""
+This profile is to be used to run OrderSage, a tool for detecting and mitigating ordering effects in systems performance experiment.
 
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
+It is specifically set up to help the user run experiments from the paper "Avoiding the Ordering Trap in Systems Performance Measurement" by Duplyakin et. al in USENIX ATC 20203
 
 Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
+TBD
 """
 
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
+# Load Emulab extensions
+import geni.rspec.emulab
 
 # Create a portal context.
 pc = portal.Context()
 
+# TODO: make these options
+WORKER_HWTYPE = "xl170"
+DISKIMAGE = " 	urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD:13"
+
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
-# Add a raw PC to the request.
-node = request.RawPC("node")
+# This node will be used as the controller
+controller = request.RawPC("contoller")
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+# ... and this one will be used as the worker
+worker = request.RawPC("contoller")
+
+# The worker is allowed to be any type of PC, but for the paper, we use a specific type for the
+# worker
+worker.hardware_type = WORKER_HWTYPE
+
+# Use a specific disk image that is known to work with the required software
+worker.disk_image = controller.disk_image = DISKIMAGE
+
+# Set up passwordless root ssh between the controller and worker
+controller.installRootKeys(private = True, public = False)
+worker.installRootKeys(private = False, public = True)
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
