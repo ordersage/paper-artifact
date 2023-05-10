f2c6bc94fe60f11a16751d0337a31415de9cb9b1
Gary Wong <gtw@flux.utah.edu>
2023-05-10 10:24:00 -0600

    Move the RSA key to a more appropriate location.

--- install-deps.sh
+++ install-deps.sh
@@ -16,8 +16,8 @@ if ! passwd -S -q ordersage; then
     sudo mkdir /home/ordersage/.ssh
     sudo sh -c 'geni-get key > /home/ordersage/.ssh/id_rsa'
     sudo chmod 600 /home/ordersage/.ssh/id_rsa
-    sudo cp /home/ordersage/.ssh/id_rsa /local/repository/
-    sudo chmod 644 /local/repository/id_rsa
+    sudo cp /home/ordersage/.ssh/id_rsa /local/
+    sudo chmod 644 /local/id_rsa
     sudo sh -c 'ssh-keygen -e -f /home/ordersage/.ssh/id_rsa > /home/ordersage/.ssh/id_rsa.pub'
     sudo sh -c 'ssh-keygen -i -f /home/ordersage/.ssh/id_rsa.pub > /home/ordersage/.ssh/authorized_keys'
     sudo chown -R ordersage:ordersage /home/ordersage/.ssh
